0b20c21a2a93e5139df40b145d882258be53bac2
Mardel Lara <alexanderchandy2018@gmail.com>
2023-03-03 11:20:09 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1Tarea1/Ejercicio1Tarea1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1Tarea1
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public int Edad { get; set; }
+
+        public string Correo { get;set; }
+    }
+}
---
+++ Ejercicio1Tarea1/Ejercicio1Tarea1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1Tarea1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviar_Clicked(object sender, EventArgs e)
+        {
+            var datos = new Class1 
+            {
+                Nombre = txtn1.Text,
+                Apellido = txtn2.Text,
+                Edad = int.Parse (txtn3.Text),
+                Correo= txtn4.Text,
+            };
+           var secondpage = new PageMostrar ();
+            secondpage.BindingContext = datos ;
+            Navigation.PushAsync(secondpage);
+
+           
+        }
+    }
+}
